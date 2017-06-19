d58d344d0f95ef32c55b06ac4867b4a41a24556a
Radoslav Genchev <r.genchew@gmail.com>
2017-06-19 12:32:33 +0000

    Dev tool initial commit

---
+++ lib/genchew_view_tool.rb
@@ -0,0 +1,6 @@
+require "genchew_view_tool/version"
+require "genchew_view_tool/renderer"
+
+module GenchewViewTool
+  # Your code goes here...
+end
---
+++ lib/genchew_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GenchewViewTool
+  VERSION = "0.1.0"
+end
